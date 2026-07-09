6e6ca5a16b50d51b1d22aad446d11ab69b7946fb
sinuksml <sinuksml@gmail.com>
2026-07-09 19:28:33 +0530

    Fix TV search bar directional focus

--- android-tv/app/src/main/java/in/sinu/gamevault/tv/MainActivity.java
+++ android-tv/app/src/main/java/in/sinu/gamevault/tv/MainActivity.java
@@ -18,7 +18,7 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    private static final String GAMEVAULT_URL = "https://sinuksml.github.io/gamevault/?tv=1";
+    private static final String GAMEVAULT_URL = "https://sinuksml.github.io/gamevault/?tv=1&appv=nav3";
     private WebView webView;
 
     @Override
